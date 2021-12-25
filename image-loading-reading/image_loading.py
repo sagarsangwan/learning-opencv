4b038f109d1cbcc53326c8949ab369b86e47bdd3
sagarsangwan <iamsagarsangwan@gmail.com>
2021-12-26 00:27:22 +0530

    displaying image

--- image-loading-reading/image_loading.py
+++ image-loading-reading/image_loading.py
@@ -1,12 +1,14 @@
 import cv2
-import numpy as np
 
 
 
 image = cv2.imread("image-loading-reading/image1.jpg", 0)
-dimenstion = image.ndim
-shape = image.shape
+# dimenstion = image.ndim
+# shape = image.shape
 # print(type(image))
-# print(image)
-print(dimenstion)
-print(shape)
+print(image)
+# print(dimenstion)
+# print(shape)
+cv2.imshow("city", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows(1)
