f63651c0543e95bc04d363b0ed34d5d244fe72b5
sagarsangwan <iamsagarsangwan@gmail.com>
2021-12-25 23:50:18 +0530

    checking dimenstion and shape of image

--- image-loading-reading/image_loading.py
+++ image-loading-reading/image_loading.py
@@ -1,7 +1,12 @@
 import cv2
+import numpy as np
 
 
-image = cv2.imread("image-loading-reading/image1.jpg")
 
-print(type(image))
-print(image)
+image = cv2.imread("image-loading-reading/image1.jpg", 0)
+dimenstion = image.ndim
+shape = image.shape
+# print(type(image))
+# print(image)
+print(dimenstion)
+print(shape)
