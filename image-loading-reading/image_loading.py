0419c710fa447ecd0492c9feb44e167da066e788
sagarsangwan <iamsagarsangwan@gmail.com>
2021-12-26 00:32:24 +0530

    resizing image

--- image-loading-reading/image_loading.py
+++ image-loading-reading/image_loading.py
@@ -3,12 +3,10 @@ import cv2
 
 
 image = cv2.imread("image-loading-reading/image1.jpg", 0)
-# dimenstion = image.ndim
-# shape = image.shape
-# print(type(image))
+dimenstion = image.ndim
+shape = image.shape
 print(image)
-# print(dimenstion)
-# print(shape)
-cv2.imshow("city", image)
+resized_image = cv2.resize(image, (500, 500))
+cv2.imshow("city", resized_image)
 cv2.waitKey(0)
-cv2.destroyAllWindows(1)
+cv2.destroyAllWindows(0)
