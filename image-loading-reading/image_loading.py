265b613ecc7bbfb900fc8fcd1b0b52b2c76ff2d7
sagarsangwan <iamsagarsangwan@gmail.com>
2021-12-26 11:51:51 +0530

    resizing all image in a folder and save each resized image

--- image-loading-reading/image_loading.py
+++ image-loading-reading/image_loading.py
@@ -1,14 +1,27 @@
 import cv2
+import glob
 
 
+# image = cv2.imread("image-loading-reading/image1.jpg", 0)
+# dimenstion = image.ndim
+# shape = image.shape
+# # print(shape)
+# resized_image = cv2.resize(image, (int(shape[1]/4), int(shape[0]/4)))
+# cv2.imshow("city", resized_image)
+# cv2.waitKey(2000)
+# cv2.destroyAllWindows()
+# # print(resized_image.shape)
+# cv2.imwrite("image-loading-reading/resized_image1.jpg", resized_image)
 
-image = cv2.imread("image-loading-reading/image1.jpg", 0)
-dimenstion = image.ndim
-shape = image.shape
-print(shape)
-resized_image = cv2.resize(image, (int(shape[1]/4), int(shape[0]/4)))
-cv2.imshow("city", resized_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-print(resized_image.shape)
-cv2.imwrite("image-loading-reading/resized_image1.jpg", resized_image)
+
+images = glob.glob("image-loading-reading/sample_images/*.jpg")
+print(images)
+i = 1
+for image in images:
+    i = i+1
+    img = cv2.imread(image, 0)
+    resized = cv2.resize(img, (100, 100))
+    cv2.imshow("img", resized)
+    cv2.waitKey(500)
+    cv2.destroyAllWindows()
+    cv2.imwrite("image-loading-reading/resized_sample_image/"+str(i)+".jpg", resized)
