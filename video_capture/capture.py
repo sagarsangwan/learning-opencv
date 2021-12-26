9ff59de5ec75e2a0b5e93c60596f50fea4d2e762
sagarsangwan <iamsagarsangwan@gmail.com>
2021-12-26 23:52:32 +0530

    playing video from webcam

--- video_capture/capture.py
+++ video_capture/capture.py
@@ -1,15 +1,15 @@
 import cv2, time
 video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-check, frame = video.read()
 
 
-print(check)
-print(frame)
+while True:
+    check, frame = video.read()
+    print(check)
+    print(frame)
+    cv2.imshow("image", frame)
+    key = cv2.waitKey(10)
+    if key == ord("s"):
+        break
 
-
-
-time.sleep(5)
-cv2.imshow("image", frame)
-cv2.waitKey(0)
 video.release()
 cv2.destroyAllWindows()
