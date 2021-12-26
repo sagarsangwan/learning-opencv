6c332cbe1208376e2e1b9a66762174b51c44a2f1
sagarsangwan <iamsagarsangwan@gmail.com>
2021-12-26 11:09:12 +0530

    saving resized image

--- image-loading-reading/image_loading.py
+++ image-loading-reading/image_loading.py
@@ -5,8 +5,10 @@ import cv2
 image = cv2.imread("image-loading-reading/image1.jpg", 0)
 dimenstion = image.ndim
 shape = image.shape
-print(image)
-resized_image = cv2.resize(image, (500, 500))
+print(shape)
+resized_image = cv2.resize(image, (int(shape[1]/4), int(shape[0]/4)))
 cv2.imshow("city", resized_image)
 cv2.waitKey(0)
-cv2.destroyAllWindows(0)
+cv2.destroyAllWindows()
+print(resized_image.shape)
+cv2.imwrite("image-loading-reading/resized_image1.jpg", resized_image)
